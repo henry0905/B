afa5879609c3aa718a43561ead7ca406c15e72a9
henry0905 <lamminhtriet1997@gmail.com>
2021-02-09 21:58:39 +0100

    Add files via upload

---
+++ 1. C for Everyone: Programming Fundamentals/Week 1/helloworld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("poem\n");
+    return 0;
+}    
---
+++ 1. C for Everyone: Programming Fundamentals/Week 1/printpoem.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("Let America be America again.\n");
+    printf("Let it be the dream it used to be.\n");
+    printf("Let it be the pioneer on the plain\n");
+    printf("Seeking a home where he himself is free.\n");
+    return 0;
+}    
